9379438d1a99e1f74b96d14d58c5d40122003831
jacqueline-98 <jacqueline-98@hotmail.com>
2023-11-13 12:33:47 +0100

    extractFilenames function added

---
+++ R/extractFilenames.R
@@ -0,0 +1,22 @@
+#' Extract file names from user prompt
+#'
+#' This function extracts file names from the user prompt
+#' @param text user prompt
+#'
+#' @examples
+#' \dontrun{
+#' extractFilenames(text=user_prompt)
+#' }
+#' @export
+extractFilenames <- function(text) {
+  # Use regular expression to match common file extensions
+  matches <- gregexpr("\\b\\S+\\.(txt|tsv|csv|xls|xlsx)\\b", text, ignore.case = TRUE)
+  filenames <- regmatches(text, matches)[[1]]
+
+  # Return filenames if found, otherwise return NA
+  if (length(filenames) == 0) {
+    return(NA)
+  } else {
+    return(filenames)
+  }
+}
