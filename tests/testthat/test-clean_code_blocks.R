453f7a465bf443d84a8b831d40a8102c32289d5c
jacqueline-98 <jacqueline-98@hotmail.com>
2023-11-14 20:08:01 +0100

    tests added

--- tests/testthat/test-clean_code_blocks.R
+++ tests/testthat/test-clean_code_blocks.R
@@ -24,3 +24,14 @@ test_that("Code blocks are clean", {
   test_obj <- "```\ninstall.packages(mergen)\ncleaned<-clean_code_blocks(code)"
   expect_equal(clean_code_blocks(test_obj), "```\n\ncleaned<-clean_code_blocks(code)")
 })
+
+test_that("Code blocks are clean and text remains", {
+  test_obj <- "I have some nice packages for you```r\ninstall.packages(mergen)\ncleaned<-clean_code_blocks(code)"
+  expect_equal(clean_code_blocks(test_obj), "I have some nice packages for you```\n\ncleaned<-clean_code_blocks(code)")
+})
+
+test_that("Code blocks are clean and text remains, so do the R and r letters within code", {
+  test_obj <- "I have some nice packages for you```r\ninstall.packages(mergen)\ncleaned_r<-clean_code_blocks(Rcode)"
+  expect_equal(clean_code_blocks(test_obj), "I have some nice packages for you```\n\ncleaned_r<-clean_code_blocks(Rcode)")
+})
+
--- tests/testthat/test-executeCode.R
+++ tests/testthat/test-executeCode.R
@@ -1,3 +1,7 @@
-test_that("Code can be executed", {
+test_that("Code blocks can be executed", {
   expect_equal(executeCode("2 * 2"), 4)
+})
+
+test_that("Code blocks can be executed", {
+  expect_equal(executeCode("\n#we are doing a t.test now\nt.test(c(1,2),c(1,2))$p.value\n\n"), t.test(c(1,2),c(1,2))$p.value )
 })
