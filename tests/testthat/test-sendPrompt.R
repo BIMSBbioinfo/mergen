c9e54a650e0893fb6415c29d49e3d18077acd5ec
jacqueline-98 <jacqueline-98@hotmail.com>
2023-11-22 11:34:29 +0100

    test added for different agents

--- tests/testthat/test-sendPrompt.R
+++ tests/testthat/test-sendPrompt.R
@@ -31,6 +31,35 @@ test_that("invalid LLM gives error",{
   expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
 })
 
+test_that("openai-agent needs to have all correct fields",{
+  # no key
+  agent<-list(name="openai",model="gpt-4",type="chat")
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+  # no type
+  agent<-list(name="openai",model="gpt-4",openai_api_key = "key")
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+  # no model
+  agent<-list(name="openai",type="chat",openai_api_key = "key")
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+})
+
+test_that("userAgent needs to have all correct fields",{
+  # no key
+  agent<-list(name="userAgent",model="llama2",url="example.com",headers=list())
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+  # no model
+  agent<-list(name="userAgent",ai_api_key="key",url="example.com",headers=list())
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+  # no url
+  agent<-list(name="userAgent",model="llama2",ai_api_key="key",headers=list())
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+  # no headers
+  agent<-list(name="userAgent",model="llama2",ai_api_key="key",url="example.com")
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+})
+
+
+
 
 test_that("testagent gives 3 responses when called 3 times",{
   agent<-list(name="testAgent",model="gpt-4",type="chat",openai_api_key=Sys.getenv("OPENAI_API_KEY"))
@@ -38,3 +67,11 @@ test_that("testagent gives 3 responses when called 3 times",{
   expect_equal(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"),"\n\nThe second response.The following R code will read the file called \"test.txt\", normalize the table and do PCA. First, the code will read the file into an R data frame: \n\n```\ndata <- read.table(\"test.txt\", header = TRUE, sep = \"\\t\")\n```\n\nNext, the data will be normalized to the range of 0 to 1:\n\n```\nnormalized.data <- scale(data, center = TRUE, scale = TRUE)\n```\n\nFinally, the normalized data will be used to do a Principal Component Analysis (PCA):\n\n```\npca <- princomp(normalized.data)\n```")
   expect_equal(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"),"\n\nThe third response.The following R code will read the file called \"test.txt\", normalize the table and do PCA. First, the code will read the file into an R data frame: \n\n```\nplot(1:10)```\n\nNext, the data will be normalized to the range of 0 to 1:\n\n")
 })
+
+
+
+test_that("error when non-existing url is used.",{
+  agent<-list(name="userAgent",model="llama2",url="example.com",taks="completion",key="key")
+  expect_error(sendPrompt(agent=agent,prompt="this is my prompt",return.type = "text"))
+})
+
