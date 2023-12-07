4fa2ba799517c2d267901aa6394f1793036da1b4
jacqueline-98 <jacqueline-98@hotmail.com>
2023-12-07 10:54:05 +0100

    test deleted

--- tests/testthat/test-extractInstallPkg.R
+++ tests/testthat/test-extractInstallPkg.R
@@ -13,12 +13,6 @@ test_argument_validation(
 )
 
 
-test_that("extractInstallPkg extracts packages from code", {
-  prompt <- "#code tells you to load a library\nlibrary(mergen)\n x<-5"
-  expect_message(extractInstallPkg(prompt))
-  })
-
-
 test_that("extractInstallPkg extracts does not extract when package is not called with library", {
   prompt <- "#code tells you to load a library\nx<-5\n mergen::extractInstallPkg()"
   expect_no_message(extractInstallPkg(prompt))
